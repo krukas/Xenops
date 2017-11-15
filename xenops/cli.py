0783288750bd1cffda730c93ee2f7df97502788d
Maikel Martens <maikel_martens_1@hotmail.com>
2017-11-15 08:21:17 +0100

    [FEATURE] Add argparse command to cli

--- xenops/cli.py
+++ xenops/cli.py
@@ -6,6 +6,7 @@ xenops.cli
 :license: GPLv3
 """
 import sys
+import argparse
 import logging
 
 from xenops.app import Application
@@ -18,15 +19,46 @@ class Command:
 
     def __init__(self, argv):
         """Init Setup Xenops"""
-        logging.basicConfig(format="%(levelname)-8s: %(message)s", level=logging.DEBUG)
+        parser = argparse.ArgumentParser(prog='xenops')
+        parser.add_argument('--verbose', '-v', action='count', default=0)
+        subparsers = parser.add_subparsers(help='sub-command help')
 
-        # TODO add argparse
+        trigger_parser = subparsers.add_parser('trigger', help='Run a trigger')
+        trigger_parser.add_argument('connector', help='Code of connector')
+        trigger_parser.add_argument('trigger', help='Code of trigger')
+        trigger_parser.add_argument('-l', '--list', dest='list', action='store_true')
+        trigger_parser.add_argument('--verbose', '-v', action='count', default=0)
+        trigger_parser.set_defaults(func=self.trigger)
 
-        # add option show flows: will show trigger -> enhances -> processes
+        args = parser.parse_args()
 
-        # Start app
+        if args.verbose:
+            levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+            level = levels[min(len(levels) - 1, args.verbose)]
+            logging.basicConfig(format="%(levelname)-8s: %(message)s", level=level)
+
+        args.func(args)
+
+    def trigger(self, args):
+        """
+        Run trigger sub command
+
+        :param argparse.Namespace args:
+        """
         app = Application()
-        app.trigger('product', 'pim_live')
+
+        if args.list:
+            print('Active triggers:\n')
+            for connector in app.connectors.values():
+                print('{} ({}):'.format(connector.code, connector.service.code))
+                for trigger_code, config in connector.triggers.items():
+                    print(' - {}'.format(trigger_code))
+                    for key, value in config.items():
+                        if key in ['type', 'trigger_code']:
+                            continue
+                        print('   - {}: {}'.format(key, value))
+        else:
+            app.trigger(args.trigger, args.connector)
 
 
 if __name__ == '__main__':
